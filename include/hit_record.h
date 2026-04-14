4e92b5170b727497e7576ca6a5febcf8cbcd9f9b
roozyfx <roozyfx@gmail.com>
2026-04-14 10:10:38 +0200

    9: HitRecord to save hit information

---
+++ include/hit_record.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_HIT_RECORD_H_
+#define INCLUDE_HIT_RECORD_H_
+#include "common.h"
+#include "ray.h"
+#include "vectormath.h"
+struct HitRecord {
+  Point3 p{Point3{INF, INF, INF}};
+  Vec3 n;
+  Float t;
+  bool front_face;
+
+  explicit HitRecord(Point3 hit_point, Vec3 normal, Float time)
+      : p(hit_point), n(normal), t(time) {}
+  void setFaceNormal(const Ray &r, Vec3 outward_normal) {
+    front_face = dot(r.direction(), outward_normal) < 0;
+    n = front_face ? outward_normal : -outward_normal;
+  }
+};
+
+#endif  // INCLUDE_HIT_RECORDE_H_
