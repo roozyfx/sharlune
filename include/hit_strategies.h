a96939537d2f2be74fc218a40b4d9c16921b73b4
roozyfx <roozyfx@gmail.com>
2026-04-14 10:15:11 +0200

    14: hit strategy for spheres

---
+++ include/hit_strategies.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_HIT_STRATEGIES_H_
+#define INCLUDE_HIT_STRATEGIES_H_
+
+struct Sphere;
+class Ray;
+struct HitRecord;
+struct Interval;
+
+bool hit_sphere(const Sphere &s, const Ray &r, const Interval &t,
+                HitRecord &record);
+
+class HitStrategy {
+ public:
+  bool operator()(const Sphere &s, const Ray &r, const Interval &t,
+                  HitRecord &record) const {
+    return hit_sphere(s, r, t, record) * 0.5;
+  }
+};
+
+#endif  // INCLUDE_HIT_STRATEGIES_H_
---
+++ src/hit_strategies.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+
+#include "basic_geometries.h"
+#include "hit_record.h"
+#include "hit_strategies.h"
+#include "interval.h"
+#include "ray.h"
+#include "vectormath.h"
+
+bool hit_sphere(const Sphere &s, const Ray &r, const Interval &tt,
+                HitRecord &record) {
+  const auto d = r.direction();
+  const auto oc = s.center - r.origin();
+
+  const auto a = length_squared(d);
+  const auto h = dot(d, oc);
+  const auto c = length_squared(oc) - s.radius * s.radius;
+
+  const auto discriminant = h * h - a * c;
+  if (discriminant < 0) return false;
+  const auto sqrtdisc = std::sqrt(discriminant);
+
+  auto root = (h - sqrtdisc) / a;
+  if (root < tt.t_min or root > tt.t_max) {
+    root = (h + sqrtdisc) / a;
+    if (root < tt.t_min or root > tt.t_max) {
+      return false;
+    }
+  }
+
+  // save hit information to record
+  record.p = r.at(root);
+  record.t = root;
+  auto outward_normal = (record.p - s.center) / s.radius;
+  record.setFaceNormal(r, outward_normal);
+
+  return true;
+}
