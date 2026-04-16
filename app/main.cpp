77c174e6af96d02d1156241fc2c1b8292b022b1d
roozyfx <roozyfx@gmail.com>
2026-04-16 10:47:10 +0200

    24: main function putting things  together

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,31 @@
 #include <cstdlib>
+#include <memory>
 
-int main() { return EXIT_SUCCESS; }
+#include "color_functions.h"
+#include "film.h"
+#include "pinhole_camera.h"
+#include "ppm.h"
+#include "render.h"
+#include "scene.h"
+#include "vectormath.h"
+
+int main() {
+  // Create the scene to render
+  auto world{create_world()};
+
+  // Create a camera
+  std::unique_ptr<Camera> camera =
+      std::make_unique<PinholeCamera>(Point3(0., 0., 0.));
+
+  // Set film (image) parameters
+  const size_t num_channels{3};
+  const size_t max_val{65535};
+  std::unique_ptr<Film> image =
+      std::make_unique<PPMImage>("spheres.ppm", camera->image_width(),
+                                 camera->image_height(), max_val, num_channels);
+
+  // Render loop
+  render(std::move(camera), std::move(image), world, sample_colorize);
+
+  return EXIT_SUCCESS;
+}
