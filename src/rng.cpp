4556abb9a425a145a84e61a40cdfee9bed602fa8
roozyfx <roozyfx@gmail.com>
2026-04-22 12:55:30 +0200

    27: Bug fix random number generator

--- src/rng.cpp
+++ src/rng.cpp
@@ -1,14 +1,23 @@
 #include <random>
 
 #include "rng.h"
+#include "vectormath.h"
 
+/*
+Generate random number in range [0, 1]
+*/
 Float random_number() {
-  static std::uniform_real_distribution<Float> distribution;
-  static std::mt19937 generator;
+  static std::uniform_real_distribution<Float> distribution(Float(0),
+                                                            Float(1.));
+  static std::random_device rd;
+  static std::mt19937 generator(rd());
 
   return distribution(generator);
 }
 
+/*
+Generate random number in range [min, max]
+*/
 Float random_number(const Float min, const Float max) {
   return min + (max - min) * random_number();
 }
