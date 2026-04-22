602a39cff9157de80750d18fd66422b52d2a1f05
roozyfx <roozyfx@gmail.com>
2026-04-22 13:00:19 +0200

    29: Add random Vec3, unit vector and on half-sphere functions

--- include/rng.h
+++ include/rng.h
@@ -2,8 +2,13 @@
 #define INCLUDE_RNG_H_
 
 #include "common.h"
+#include "vectormath.h"
 
 Float random_number();
-
 Float random_number(const Float min, const Float max);
+
+Vec3 random_vec3(const Float min, const Float max);
+
+Vec3 random_unit_vector();
+Vec3 random_on_half_sphere(const Vec3& normal);
 #endif  // INCLUDE_RNG_H_
--- src/rng.cpp
+++ src/rng.cpp
@@ -21,3 +21,27 @@ Generate random number in range [min, max]
 Float random_number(const Float min, const Float max) {
   return min + (max - min) * random_number();
 }
+
+/*
+Generate random Vec3 with each element in range [min, max]
+*/
+Vec3 random_vec3(const Float min, const Float max) {
+  return Vec3(random_number(min, max), random_number(min, max),
+              random_number(min, max));
+}
+
+// Generate a uniformly distributed unit vector on the unit-sphere
+Vec3 random_unit_vector() {
+  while (true) {
+    // generate a random vector in [-1, -1] cube
+    Vec3 v = random_vec3(-1, 1);
+    if (length_squared(v) >= 1e-160 and length_squared(v) <= 1) {
+      return normalize(v);
+    }
+  }
+}
+
+Vec3 random_on_half_sphere(const Vec3 &normal) {
+  auto u{random_unit_vector()};
+  return dot(u, normal) > Float(0.) ? u : -u;
+}
