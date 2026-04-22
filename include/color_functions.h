62d01b2a9067e962636e89379d3512c8774cc282
roozyfx <roozyfx@gmail.com>
2026-04-22 16:44:12 +0200

    34: Added a lambertian diffuser color function

--- include/color_functions.h
+++ include/color_functions.h
@@ -10,4 +10,6 @@ Color sample_colorize(const Ray& r, const std::shared_ptr<Hittables>& world,
                       size_t depth);
 Color naive_diffuse(const Ray& r, const std::shared_ptr<Hittables>& world,
                     size_t depth);
+Color lambertian_diffuse(const Ray& r, const std::shared_ptr<Hittables>& world,
+                         size_t depth);
 #endif  // INCLUDE_COLOR_FUNCTIONS_H_
--- src/color_functions.cpp
+++ src/color_functions.cpp
@@ -32,3 +32,20 @@ Color naive_diffuse(const Ray& r, const std::shared_ptr<Hittables>& world,
   auto a = 0.5 * (unit_direction.y + 1.0);
   return ((1.0 - a) * Color(0., 0., 1.) + a * Color(1., 1.0, 1.0));
 }
+
+Color lambertian_diffuse(const Ray& r, const std::shared_ptr<Hittables>& world,
+                         size_t depth) {
+  if (depth == 0) return Color(0, 0, 0);
+  HitRecord rec;
+  // Set the min interval to slightly larger than 0, to prevent 'trapped'
+  // bounces due to floating-point rounding error.
+  if (world->hit_all(r, Interval(0.001, std::numeric_limits<Float>::max()),
+                     rec)) {
+    Vec3 direction = rec.n + random_unit_vector();
+    return naive_diffuse(Ray(rec.p, direction), world, depth - 1) * 0.5;
+  }
+
+  Vec3 unit_direction = normalize(r.direction());
+  auto a = 0.5 * (unit_direction.y + 1.0);
+  return ((1.0 - a) * Color(0., 0., 1.) + a * Color(1., 1.0, 1.0));
+}
