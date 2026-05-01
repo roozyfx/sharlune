dc45a8f1f5de656a2fd2d39afc9a66f5e14915e7
roozyfx <roozyfx@gmail.com>
2026-05-01 13:35:59 +0200

    42: Added Material variant and Scatter visitors

---
+++ include/material.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_MATERIAL_H_
+#define INCLUDE_MATERIAL_H_
+#include <variant>
+
+#include "vectormath.h"
+
+// TODO
+struct Metal {
+  Color albedo;
+};
+struct Lambertian {
+  Color albedo;
+};
+
+using Material = std::variant<Metal, Lambertian>;
+
+#endif  // INCLUDE_COLOR_FUNCTIONS_H_
---
+++ include/scatter.h
@@ -0,0 +1,34 @@
+#ifndef INCLUDE_SCATTER_H_
+#define INCLUDE_SCATTER_H_
+
+#include "hit_record.h"
+#include "material.h"
+#include "ray.h"
+#include "rng.h"
+#include "vectormath.h"
+
+static bool b_lambert{true};
+static bool b_metal{true};
+struct Scatter {
+  bool operator()(const Metal &material, const Ray &in_ray,
+                  const HitRecord &rec, Color &attenuation,
+                  Ray &out_ray) const {
+    Vec3 reflected = reflect(in_ray.direction(), rec.n);
+    out_ray = Ray(rec.p, reflected);
+    attenuation = material.albedo;
+
+    return true;
+  }
+
+  bool operator()(const Lambertian &material, const Ray &, const HitRecord &rec,
+                  Color &attenuation, Ray &out_ray) const {
+    // TODO correct and complete lambertian
+    auto scatter_direction = rec.n + random_unit_vector();
+    out_ray = Ray(rec.p, scatter_direction);
+    attenuation = material.albedo;
+
+    return true;
+  }
+};
+
+#endif  // INCLUDE_SCATTER_H_
